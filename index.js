7b46dd235b0281497d1fb3162e66834b48ce32f4
Aderinola Opemipo Emmanuel <aderinolaopemipo@gmail.com>
2023-09-07 01:53:51 +0100

    added the links loop to the settimeout function

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ setTimeout(() => {
     hamburger.addEventListener("click", ()=> {
         navLinks.classList.toggle("show");
     });
+    
+    for (let i = 0; i < 4; i++) {
+        links[0].addEventListener("click", ()=> {
+             navLinks.classList.remove("show");
+        });
+        links[1].addEventListener("click", ()=> {
+             navLinks.classList.remove("show");
+        });
+        links[2].addEventListener("click", ()=> {
+             navLinks.classList.remove("show");
+        });
+        links[3].addEventListener("click", ()=> {
+             navLinks.classList.remove("show");
+        });    
+    }
 }, 300);
-
-for (let i = 0; i < 4; i++) {
-    links[0].addEventListener("click", ()=> {
-         navLinks.classList.remove("show");
-    });
-    links[1].addEventListener("click", ()=> {
-         navLinks.classList.remove("show");
-    });
-    links[2].addEventListener("click", ()=> {
-         navLinks.classList.remove("show");
-    });
-    links[3].addEventListener("click", ()=> {
-         navLinks.classList.remove("show");
-    });
-     
- }
