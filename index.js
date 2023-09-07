f461d44f7bad235ac5d016e093f25a83a635cce2
Aderinola Opemipo Emmanuel <aderinolaopemipo@gmail.com>
2023-09-07 01:48:59 +0100

    edited js to close navmenu when a link is clicked

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 let hamburger = document.querySelector(".hamburger");
 let navLinks = document.querySelector(".navlinks");
+let links = document.querySelectorAll(".link");
 
 setTimeout(() => {
     hamburger.addEventListener("click", ()=> {
         navLinks.classList.toggle("show");
     });
 }, 300);
+
+for (let i = 0; i < 4; i++) {
+    links[0].addEventListener("click", ()=> {
+         navLinks.classList.remove("show");
+    });
+    links[1].addEventListener("click", ()=> {
+         navLinks.classList.remove("show");
+    });
+    links[2].addEventListener("click", ()=> {
+         navLinks.classList.remove("show");
+    });
+    links[3].addEventListener("click", ()=> {
+         navLinks.classList.remove("show");
+    });
+     
+ }
